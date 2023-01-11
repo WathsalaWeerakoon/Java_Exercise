0ed8b37fccc42c5a297b7b287e4871adea78475b
WathsalaWeerkoon <wdweerakoon96@gmail.com>
2023-01-11 10:48:27 +0530

    first commit

--- src/Java_Logic_Exercise/Standard_Deviation.java
+++ src/Java_Logic_Exercise/Standard_Deviation.java
@@ -3,8 +3,53 @@ package Java_Logic_Exercise;
 public class Standard_Deviation {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-
-	}
-
+		
+		int [] numbers = {1,2,3,4,5,6,7,8,9,10};
+		
+		
+        int length = numbers.length;
+ 
+       
+        int sum = 0;
+ 
+       
+        for (int i = 0; i < numbers.length; i++) 
+        {
+            sum += numbers[i];
+        }
+ 
+        double average = sum / length;
+         
+        System.out.println("Average of array : "+average);
+        System.out.println();
+		
+     
+        double sqDiff = 0;
+        
+        for (int i = 0; i < numbers.length; i++)
+        	
+        	{
+        	sqDiff += (numbers[i] - average) *(numbers[i] - average);
+        	
+        	
+        	
+        	
+        }
+        
+        double sd = sqDiff / numbers.length;
+    	
+    	System.out.println("Standars deviation : "+sd);
+    	
+    	double sQSd = Math.sqrt(sd);
+    	
+    	System.out.println();
+    	System.out.println("Squre Root of Standars deviation : "+sQSd);
+    	
+    }
+     
+    
 }
+
+
